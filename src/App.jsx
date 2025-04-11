79d18655415cd46e946a8a2b5e4e918046ddc0c6
Bruno Jorba <brujorba@gmail.com>
2025-04-10 21:26:05 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav className='navBar'>
+      NavBar
+    </nav>
+    <section className='home'>
+      Home
+    </section>
+    <section className='aboutUs'>
+      About us
+    </section>
+    <section className='projects'>
+      Projects
+    </section>
+    <section className='contactUs'>
+      Contact us
+    </section>
+    </>
+  )
+}
+
+export default App
